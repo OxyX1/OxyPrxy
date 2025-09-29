587697848a347ed9b7cb89b96d308f1d17e27635
Fluxgate <29GariglioSe@fortcherry.org>
2025-09-29 03:43:10 +0000

    PWA fix

--- sw.js
+++ sw.js
@@ -1,15 +1,21 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("my-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        // add all your assets
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
     })
   );
 });
